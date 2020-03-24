e9665bd21ff757cef02223a46f10c4b16bc470d0
Robert Candler <robert.candler.32@gmail.com>
2020-03-24 15:14:13 -0600

    Adding all files

---
+++ questions.js
@@ -0,0 +1,62 @@
+var questions = [
+    //Q1
+    {
+		title: "Which of these is not an animal?",
+		choices: ["cat", "dog", "bird", "tree"],
+		answer: "tree"
+    },
+    //Q2
+	{
+		title: "Which of these is a State?",
+		choices: ["Guam", "Kansas", "American Samoa", "Puerto Rico"],
+		answer: "Kansas"
+    },
+    //Q3
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+    //Q4
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+    //Q5
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+    //Q6
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+    //Q7
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+    //Q8
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+    //Q9
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+    //Q10
+    {
+		title: "",
+		choices: ["", "", "", ""],
+		answer: ""
+    },
+]
