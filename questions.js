32420c9afdeeea0e7c354f2042388cd0edaf217e
Robert Candler <robert.candler.32@gmail.com>
2020-03-24 15:55:49 -0600

    Finished writing questions

--- questions.js
+++ questions.js
@@ -13,50 +13,50 @@ var questions = [
     },
     //Q3
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "What color is the sky?",
+		choices: ["Blue", "neon", "white", "purple"],
+		answer: "Blue"
     },
     //Q4
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "Which of these is a javascript file?",
+		choices: ["index.html", "logic.js", "README.md", "style.css"],
+		answer: "logic.js"
     },
     //Q5
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "Which of these is normally found in an image tag?",
+		choices: ["cat", "dog", "pigeon", "src"],
+		answer: "src"
     },
     //Q6
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "Which word is spelled incorrectly in every dictionary",
+		choices: ["Robert", "grey", "wolfe", "incorrectly"],
+		answer: "incorrectly"
     },
     //Q7
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "What gets wetter the more it dries?",
+		choices: ["frozen food", "towel", "fish", "my emotions"],
+		answer: "towel"
     },
     //Q8
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "What starts with “e” and ends with “e” but only has one letter in it?",
+		choices: ["envelope", "ene", "elle", "ice"],
+		answer: "envelope"
     },
     //Q9
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "If you answer randomly, what is the probability it will be correct?",
+		choices: ["25%", "33%", "50%", "I don't care."],
+		answer: "25%"
     },
     //Q10
     {
-		title: "",
-		choices: ["", "", "", ""],
-		answer: ""
+		title: "which color is not on the rainbow?",
+		choices: ["cheese", "red", "black", "yellow"],
+		answer: "black"
     },
 ]
