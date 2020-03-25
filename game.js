6eedaaedae2d2887d3b3229bd3e3656cabae2602
robertrcandler <61333277+robertrcandler@users.noreply.github.com>
2020-03-24 18:36:48 -0600

    Updated

---
+++ game.js
@@ -0,0 +1,7 @@
+function checkAnswer() {
+    alert("Checking");
+    //Make Question Result Visible
+    document.getElementById("qResult").style.visibility = "visible";
+}
+
+//https://www.youtube.com/watch?v=C7NsIRhoWuE
